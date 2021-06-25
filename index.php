b0bff7af8cb67893fbee964966a21bf7fefaf3ed
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-06-25 19:21:43 +0100

    Build error fixed

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>JF Videography</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color: rgb(122, 122, 122);">
+
+<div class="container-fluid">
+    <h1 class="text-danger">JF Videography <small class = "text-muted" id = "smallobject">We tell stories</small></h1>
+</div>
+
+</body>
+</html>
